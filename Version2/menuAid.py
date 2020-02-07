8930e3a2f8cdf31e0e413c1889700439e4ed69e8
Matthew Stapleton <ofnofixedabode@hotmail.com>
2020-02-07 10:17:17 +0000

    Added text element to easily draw text on the screen

--- Version2/menuAid.py
+++ Version2/menuAid.py
@@ -14,7 +14,6 @@ class Menu(object):
   
   def draw(self):
     self.screen.fill(self.colour)
-
     
   def update(self):
     for event in pygame.event.get():
@@ -34,5 +33,37 @@ class Element(object):
     self.width = width
     self.height = height
 
-  def inBox(self, x, y, width, height, mousePos):
+  def inBox(self, mousePos):
     return self.x < mousePos[0] < self.x + self.width and self.y < mousePos[1] < self.y + self.height
+
+  def setPos(self, x, y):
+    self.x = x
+    self.y = y
+  def setX(self, x):
+    self.x = x
+  def setY(self, y):
+    self.y = y
+
+  def getPos(self):
+    return self.x, self.y
+
+class Text(Element):
+
+  def __init__(self, x, y, text, fontName, size, colour):
+    
+    self.text = text
+    self.fontName = fontName
+    self.size = size
+    self.colour = colour
+
+    self.render()
+
+    super().__init__(x, y, self.renderedText.get_width(), self.renderedText.get_height)
+
+  def getText(self):
+    return self.renderedText
+
+  def render(self):
+    self.font = pygame.font.SysFont(self.fontName, self.size)
+    self.renderedText = self.font.render(self.text, True, self.colour)
+
--- Version2/test.py
+++ Version2/test.py
@@ -3,7 +3,11 @@ from menuAid import *
 
 m = Menu(600, 600, (60, 60, 60))
 
+m.testText = Text(0, 0, "Test", "arial", 20, (255, 255, 255))
+
 while 1:
   m.draw()
+
+  m.screen.blit(m.testText.getText(), m.testText.getPos())
 
   m.update()
